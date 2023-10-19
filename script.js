aa20fa986f8b8af0f1923cb5015d4b7ecc57d091
Yure Rafael <rafaelyure53@gmail.com>
2023-10-18 21:09:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let playersSelected = []
+
+const sectionCards = document.getElementById('container-cards')
+const cards = sectionCards.children
+const players = [...cards] 
+
+const addPlayer = () => {
+    const board = document.getElementById('board')
+    players.map((player) => {
+        const createSpace = document.createElement('div')
+        createSpace.classList = 'space-player'
+        player.onclick = () => {
+            if(playersSelected.length === 5) alert('voce ja escolheu 5 jogadores!')
+            if(playersSelected.includes(player.id) || playersSelected.length === 5) return    
+
+            playersSelected.push(player.id)
+            
+            const imgPlayer = document.createElement('img')
+            imgPlayer.src = player.src
+            
+            const namePlayer = document.createElement('h2')
+            namePlayer.innerHTML = player.id
+            
+            createSpace.id = player.id
+            createSpace.appendChild(imgPlayer)
+            createSpace.appendChild(namePlayer)
+            board.appendChild(createSpace)
+        }
+    })
+}
+
+const removePlayer = () => {
+    const board = document.getElementById('board')
+    const playersBoard = [...board.children]
+    playersBoard.map((player) => {
+        player.onclick = () => {
+            playersSelected = playersSelected.filter(item => item !== player.id)
+            const elementToRemove = player
+            elementToRemove.remove()
+        }
+    })
+}
+
+addPlayer()
